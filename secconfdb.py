25ce3edfca1f59614afb3e55ea64a4bcd18aa130
Jonathan Anderson <jonathan.anderson@ieee.org>
2011-07-12 16:44:11 +0100

    Properly format both conference and deadline vCals

--- secconfdb.py
+++ secconfdb.py
@@ -41,10 +41,57 @@ def hello():
 			soonness = soonness)
 
 
-@app.route('/calendar.ics')
-def calendar():
+
+def make_calendar(events, title):
 	template = jinja.get_template('vcal')
-	return template.render(events = db.defaultQuery(), datetime = datetime)
+	return flask.Response(
+		response = template.render(
+			events = events, title = title, datetime = datetime),
+		mimetype = 'text/plain')
+
+@app.route('/conferences.ics')
+def conference_calendar():
+	events = [
+		(
+			event.abbreviation, event.name, event.where(),
+			(event.startDate, event.endDate)
+		)
+		for event in db.upcoming()
+	]
+
+	return make_calendar(events, 'Upcoming Conferences')
+
+@app.route('/deadlines.ics')
+def deadline_calendar():
+	events = []
+	for conf in db.deadlines():
+		if conf.extendedDeadline:
+			events.append(
+				(
+					"%s Deadline (extended)" % conf.abbreviation,
+					"Extended paper deadline for %s" % conf.name,
+					conf.where(),
+					conf.extendedDeadline,
+				))
+		elif conf.deadline:
+			events.append(
+				(
+					"%s Deadline" % conf.abbreviation,
+					"Paper deadline for %s" % conf.name,
+					conf.where(),
+					conf.deadline,
+				))
+
+		if conf.posterDeadline:
+			events.append(
+				(
+					"%s Poster Deadline" % conf.abbreviation,
+					"Poster deadline for %s" % conf.name,
+					conf.where(),
+					conf.posterDeadline,
+				))
+
+	return make_calendar(events, 'Conference Deadlines')
 
 
 # Conference info, either by ID or by abbreviation.
