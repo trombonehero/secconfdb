ff3ad8fabc31c0f908952ae7aa1e9b9f82c1ffc1
Jonathan Anderson <jonathan.anderson@ieee.org>
2011-07-12 11:55:26 +0100

    Beginnings of vCal stuff

--- secconfdb.py
+++ secconfdb.py
@@ -1,3 +1,4 @@
+import datetime
 import db
 
 import flask
@@ -7,21 +8,31 @@ app = flask.Flask(__name__)
 
 jinja = jinja2.Environment(loader = jinja2.PackageLoader('secconfdb', 'layout'))
 
-row = jinja2.Template("""
+row = jinja2.Template('''
 <tr>
 	<td>{{ conf.abbreviation }}</td>
 	<td>{{ conf.when() }}</td>
 	<td>{{ conf.where() }}</td>
 </tr>
-""")
+''')
 
-@app.route("/")
+@app.route('/')
 def hello():
-	template = jinja.get_template("main.html")
+	template = jinja.get_template('main.html')
 	return template.render(conferences = db.defaultQuery(), year = 2011)
 
 
-if __name__ == "__main__":
+
+@app.route('/calendar.ics')
+def calendar():
+	template = jinja.get_template('vcal')
+	return template.render(events = db.defaultQuery(), datetime = datetime)
+
+@app.route('/conference/<int:conf_id>')
+def conference(conf_id):
+	return "Let's look up conference %d" % conf_id
+
+if __name__ == '__main__':
 	for conf in db.defaultQuery():
 		print repr(conf)
 	app.run(debug = True)
